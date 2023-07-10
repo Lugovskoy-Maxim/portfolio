8a79ba90367985caedc0f456fabcb5f39990cdf6
Максим Луговской <lugovskou.myu@yandex.ru>
2023-07-10 06:36:37 +0600

    fix: images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,10 @@
+import blog from '../images/blog.png';
+import exploper from '../images/explorer.png';
+import exploper_api from '../images/explorer-api.png';
+import mesto from '../images/mesto.png';
+import travel from '../images/travel.png';
+import learn from '../images/learn.png';
+
 export const initialProjects = [
   {
     name: 'partfolio-blog',
@@ -6,14 +13,14 @@ export const initialProjects = [
     Стек технологий: Js, React, Redux
     Основная цель: полигон для изучения новых технологий и поведения браузера.
     `,
-    image: 'https://i.ibb.co/jfNjQ9d/2023-07-10-041904.png',
+    image: `${blog.src}`,
     link: 'https://github.com/Lugovskoy-Maxim/partfolio-blog-react',
   },
   {
     name: 'exploper',
     about: 'Обозреватель фильмов',
     image: 'https://i.ibb.co/q03Qb8L/2023-07-10-042548.png',
-    link: 'https://github.com/Lugovskoy-Maxim/movies-explorer-frontend',
+    link: `${exploper.src}`,
     about_full: `Сайт с фильмами
     Стек технологий: Js, React
     Основная цель: создание полноценного приложения состоящего из 2 частей фронта и бека.
@@ -23,7 +30,7 @@ export const initialProjects = [
   {
     name: 'explorer-api',
     about: 'Серверное приложение',
-    image: 'https://i.ibb.co/WzVsTYQ/2023-07-10-043227.png',
+    image: `${exploper_api.src}`,
     link: 'https://github.com/Lugovskoy-Maxim/movies-explorer-api',
     about_full: `Серверное приложение.
     Стек технологий: Js, Express.js, Node.js
@@ -34,7 +41,7 @@ export const initialProjects = [
   {
     name: 'react-mesto-api',
     about: 'Веб приложение с фото',
-    image: 'https://i.ibb.co/HGfKshL/2023-07-10-043859.png',
+    image: `${mesto.src}`,
     link: 'https://github.com/Lugovskoy-Maxim/react-mesto-api-full',
     about_full: `React Mesto API Full
     Стек технологий: Js, React, Express.js, Node.js
@@ -43,7 +50,7 @@ export const initialProjects = [
   {
     name: 'russian-travel',
     about: 'Путешествие по России',
-    image: 'https://i.ibb.co/tbb0Lt0/2023-07-10-044028.png',
+    image: `${travel.src}`,
     link: 'https://github.com/Lugovskoy-Maxim/russian-travel',
     about_full: `Сайт о путешествиях по России
     Стек технологий: HTML, CSS, BEM
@@ -53,7 +60,7 @@ export const initialProjects = [
   {
     name: 'how-to-learn',
     about: 'Научиться учиться',
-    image: 'https://i.ibb.co/182Jjt2/2023-07-10-044037.png',
+    image: `${learn.src}`,
     link: 'https://github.com/Lugovskoy-Maxim/how-to-learn',
     about_full: `Одностроничный сайт
     Стек технологий: HTML, CSS, BEM
