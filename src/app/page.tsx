9a70f94d91a1c4438fc644d89b89689e6673ef7b
Максим Луговской <lugovskou.myu@yandex.ru>
2025-03-11 13:06:32 +0500

    add: user info & quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,7 @@ export default function Home() {
   return (
     <div className="flex items-start flex-col justify-items-center min-h-full font-[family-name:var(--font-geist-sans)] relative" >
       <main className="flex justify-items-center mt-0 ml-auto mr-auto items-start">
-      123 
-      123 
-      123 
-      123 
-      123      123 
-
-      123      123 
-      123 
-      123 
-      123 v
-      123 
-      123 
-
-      123      123 
-      123      123 
-      123 
-      <br/>
-      123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>  123 
-      <br/>
+      
       </main>
       <footer className="flex justify-items-center mt-0 ml-auto mr-auto items-start">
         1223
