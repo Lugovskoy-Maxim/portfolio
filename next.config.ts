88250e370a2a2d354b3c1d8d945de188c541adfc
Максим Луговской <lugovskou.myu@yandex.ru>
2025-06-04 23:58:34 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   basePath: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}` : '/', 
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}/` : '/', 
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}` : '/', 
   
   // experimental: {
   //   optimizeCss: true, // Активирует оптимизацию CSS
